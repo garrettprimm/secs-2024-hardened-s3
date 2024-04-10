e45e50b4955c25709340419ce21f48c21f4ecdcb
Garrett Primm <103901383+garrettprimm@users.noreply.github.com>
2024-04-09 20:42:51 -0500

    fixing security issues

--- main.tf
+++ main.tf
@@ -19,34 +19,34 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
-# resource "aws_s3_bucket_public_access_block" "main" {
-#   bucket                  = aws_s3_bucket.main.id
-#   block_public_policy     = true
-#   block_public_acls       = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  block_public_policy     = true
+  block_public_acls       = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
-#   bucket = aws_s3_bucket.main.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "aws:kms"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
 
-# resource "aws_s3_bucket_logging" "main" {
-#   bucket = aws_s3_bucket.main.id
+resource "aws_s3_bucket_logging" "main" {
+  bucket = aws_s3_bucket.main.id
 
-#   target_bucket = "blah"
-#   target_prefix = "log/"
-# }
+  target_bucket = "blah"
+  target_prefix = "log/"
+}
 
-# resource "aws_s3_bucket_versioning" "main" {
-#   bucket = aws_s3_bucket.main.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
